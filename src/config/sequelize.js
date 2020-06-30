a69dc1d7a86774fa18db1e1f3a0c1f3919dcf839
Gabriel <gabrielnabas@id.uff.br>
2020-06-30 10:20:10 -0300

    options-db-ssl-true

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -19,5 +19,8 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
